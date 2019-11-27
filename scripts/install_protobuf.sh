9c0ac8a3ccda0a72a37240897ba5f797d587b409
Hoda Mohammadi <hoda.muhammadi@td.com>
2019-11-26 22:15:35 -0500

    protobuf script

---
+++ scripts/install_protobuf.sh
@@ -0,0 +1,12 @@
+PROTOBUF_VERSION=3.11.0
+
+go get -u github.com/golang/protobuf/proto
+go get -u github.com/golang/protobuf/protoc-gen-go
+
+curl -L https://github.com/google/protobuf/releases/download/v{$PROTOBUF_VERSION}/protoc-{$PROTOBUF_VERSION}-linux-x86_64.zip -o /tmp/protoc.zip
+unzip /tmp/protoc.zip
+sudo cp ./tmp/bin/protoc /usr/local/bin/.
+sudo cp -r ./tmp/include /usr/local/.
+sudo chmod 755 /usr/local/bin/protoc
+sudo chmod -R 755 /usr/local/include/google
+export PATH=$HOME/go/bin:$PATH
