4da14d3762e00ea5619814986220ff4f4b8a93ec
Hoda Mohammadi <mohamhoda@gmail.com>
2020-01-16 21:49:24 -0500

    reorder install

--- proto/generate_proto.sh
+++ proto/generate_proto.sh
@@ -1,2 +1,2 @@
-protoc -I. -I ${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/ --go_out=plugins=grpc:. --grpc-gateway_out=logtostderr=true:. \
+protoc -I. -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/ --go_out=plugins=grpc:. --grpc-gateway_out=logtostderr=true:. \
 proto/usermanagement/user_management.proto --swagger_out=json_names_for_fields=true:.
