e84fcc5d6cd5cb5c9f85e38658ef10cbe5e4cc51
Hoda Mohammadi <mohamhoda@gmail.com>
2020-01-16 22:37:26 -0500

    gopath

--- proto/generate_proto.sh
+++ proto/generate_proto.sh
@@ -1,2 +1,5 @@
-protoc -I. -I $HOME/gopath/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/ --go_out=plugins=grpc:. --grpc-gateway_out=logtostderr=true:. \
+echo $PWD
+echo $GOPATH
+
+protoc -I. -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/ --go_out=plugins=grpc:. --grpc-gateway_out=logtostderr=true:. \
 proto/usermanagement/user_management.proto --swagger_out=json_names_for_fields=true:.
