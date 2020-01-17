5a63427fada4db6ea580256f962d747d5284bda7
Hoda Mohammadi <mohamhoda@gmail.com>
2020-01-16 19:33:59 -0500

    update proto to include http endpoints

--- proto/generate_proto.sh
+++ proto/generate_proto.sh
@@ -1,1 +1,2 @@
-protoc -I ./ proto/usermanagement/user_management.proto --go_out=plugins=grpc:./
+protoc -I. -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/ --go_out=plugins=grpc:. --grpc-gateway_out=logtostderr=true:. \
+proto/usermanagement/user_management.proto --swagger_out=json_names_for_fields=true:.
