9b2224c9f31a0be930a36d8f838da2709dda2d8b
Hoda Mohammadi <mohamhoda@gmail.com>
2020-01-16 21:11:02 -0500

    Merge branch 'master' of https://github.com/RoomFlow/backend into issue-10-api-gateway

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/RoomFlow/backend
+
+go 1.13
+
+require (
+	cloud.google.com/go v0.51.0 // indirect
+	cloud.google.com/go/firestore v1.1.1
+	firebase.google.com/go v3.11.1+incompatible
+	github.com/hashicorp/golang-lru v0.5.3 // indirect
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/sys v0.0.0-20200113162924-86b910548bc1 // indirect
+	google.golang.org/api v0.15.0
+	google.golang.org/genproto v0.0.0-20200113173426-e1de0a7b01eb // indirect
+	google.golang.org/grpc v1.26.0
+)
---
+++ services/schedule/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"github.com/RoomFlow/backend/services/schedule/helpers"
+	"log"
+)
+
+func main() {
+	// Fetch room data from McMaster room directory
+	rooms := helpers.FetchRoomData()
+
+	// Initialize new firestore client
+	client := helpers.NewFirestoreClient()
+	defer client.Close()
+
+	log.Println("Creating new batch write")
+
+	// Create new batch write so we can avoid creating one write per room
+	batch := client.Batch()
+
+	// Collection which will hold the room data
+	collRef := client.Collection("rooms")
+
+	// Iterate through room data and add new document to batch
+	for _, data := range rooms {
+		docRef := collRef.Doc(data.ID)
+		batch.Set(docRef, data)
+	}
+
+	log.Println("Committing batch write")
+
+	// Commit the batch.
+	_, err := batch.Commit(context.TODO())
+	if err != nil {
+		log.Fatalf("An error has occurred: %s", err)
+	}
+
+	log.Println("Done updating room data")
+}
