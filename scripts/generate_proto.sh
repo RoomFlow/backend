f52277122fdead1ddc41e13bc5b47f0381f968af
Karlo Delalic <karlo.delalic1@gmail.com>
2020-01-18 18:56:52 -0500

    Fix generate_proto.sh

--- scripts/generate_proto.sh
+++ scripts/generate_proto.sh
@@ -1,11 +1,12 @@
 PROTO_FILE_PATHS=`find internal/proto -name "*.proto"`
 
-find $GOPATH/src -name "grpc-gateway*" | tail -1
+GRPC_GATEWAY_PATH=`find $GOPATH/pkg/mod/github.com/grpc-ecosystem -name "grpc-gateway*" | tail -1`
+GOOGLE_APIS=$GRPC_GATEWAY_PATH/third_party/googleapis
 
 for PROTO_FILE_PATH in $PROTO_FILE_PATHS; do
     protoc -I/usr/local/include -I. \
-        -I$GOPATH/src \
-        -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+        -I$GOPATH/mod \
+        -I$GOOGLE_APIS \
         --go_out=plugins=grpc:. \
         $PROTO_FILE_PATH --swagger_out=json_names_for_fields=true:.
 done
