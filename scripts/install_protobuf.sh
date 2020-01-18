58de9fd39b8e3b61b9d229e5fc3716531fce002b
Karlo Delalic <karlo.delalic1@gmail.com>
2020-01-18 18:37:39 -0500

    Install grpc-gateway

--- scripts/install_protobuf.sh
+++ scripts/install_protobuf.sh
@@ -9,5 +9,12 @@ sudo cp ./bin/protoc /usr/local/bin/.
 sudo cp -r ./include /usr/local/.
 sudo chmod 755 /usr/local/bin/protoc
 sudo chmod -R 755 /usr/local/include/google
+which protoc
+
+git clone https://github.com/grpc-ecosystem/grpc-gateway.git
+cd grpc-gateway
+sudo cp -R ./protoc-gen-swagger /usr/local/include/.
+sudo chmod -R 755 /usr/local/include/protoc-gen-swagger
+
 export PATH=$HOME/go/bin:$PATH
 export PATH=$HOME/protoc/bin:$PATH
