e3b5a3871bd9fb7f0761eca40994fa36d7c539b2
Karlo Delalic <karlo.delalic1@gmail.com>
2020-01-18 19:06:36 -0500

    Install grpc-gateway

--- scripts/generate_proto.sh
+++ scripts/generate_proto.sh
@@ -1,7 +1,17 @@
-PROTO_FILE_PATHS=`find internal/proto -name "*.proto"`
+git clone https://github.com/grpc-ecosystem/grpc-gateway.git
+cd grpc-gateway
+sudo cp -R ./protoc-gen-swagger /usr/local/include/.
+sudo chmod -R 755 /usr/local/include/protoc-gen-swagger
+
+go get github.com/grpc-ecosystem/grpc-gateway
+go get -u github.com/golang/protobuf/protoc-gen-go
+go get github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
+go get github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
 
 GRPC_GATEWAY_PATH=`go list -m -f '{{.Dir}}' github.com/grpc-ecosystem/grpc-gateway`
 GOOGLE_APIS=$GRPC_GATEWAY_PATH/third_party/googleapis
+
+PROTO_FILE_PATHS=`find internal/proto -name "*.proto"`
 
 for PROTO_FILE_PATH in $PROTO_FILE_PATHS; do
     protoc -I/usr/local/include -I. \
--- scripts/install_protobuf.sh
+++ scripts/install_protobuf.sh
@@ -8,12 +8,3 @@ sudo chmod 755 /usr/local/bin/protoc
 sudo chmod -R 755 /usr/local/include/google
 which protoc
 protoc --version
-
-git clone https://github.com/grpc-ecosystem/grpc-gateway.git
-cd grpc-gateway
-sudo cp -R ./protoc-gen-swagger /usr/local/include/.
-sudo chmod -R 755 /usr/local/include/protoc-gen-swagger
-
-go get -u github.com/golang/protobuf/protoc-gen-go
-go get github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
-go get github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
