0244602816f29e6716f94b0ec4c85ad2596d7965
Karlo Delalic <karlo.delalic1@gmail.com>
2020-01-18 19:22:21 -0500

    Fix comments

--- scripts/generate_proto.sh
+++ scripts/generate_proto.sh
@@ -9,17 +9,17 @@ GOOGLE_APIS=$GRPC_GATEWAY_PATH/third_party/googleapis
 PROTO_FILE_PATHS=`find internal/proto -name "*.proto"`
 
 for PROTO_FILE_PATH in $PROTO_FILE_PATHS; do
+    # Generate grpc stub
     protoc -I/usr/local/include -I. \
         -I$GOPATH/src \
         -I$GOOGLE_APIS \
         --go_out=plugins=grpc:. \
         $PROTO_FILE_PATH
 
-    # Create reverse-proxy will be used for api-gateway
+    # Generate reverse proxy
     protoc -I/usr/local/include  -I. \
         -I$GOPATH/src \
         -I$GOOGLE_APIS \
         --grpc-gateway_out=logtostderr=true:. \
         $PROTO_FILE_PATH --swagger_out=json_names_for_fields=true:.
-
 done
