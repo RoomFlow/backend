ba0895f961a831d1a542f1f13eddb408c15d730e
Karlo Delalic <karlo.delalic1@gmail.com>
2020-01-23 01:56:48 -0500

    comment

--- services/firebase/functions/deploy.sh
+++ services/firebase/functions/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-### The following section will push your container image to ECR. The `$NAME` variable is provided from our
-### Makefile under 'deploy:' rule, which is set to the name of the component/module/service.
-###
+# The following deploys the firebase function
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     npm run deploy
