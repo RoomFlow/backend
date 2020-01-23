cca303d823e7f13dd65af3d0bcc52b7166edd171
Karlo Delalic <karlo.delalic1@gmail.com>
2020-01-23 02:39:06 -0500

    Fix automation

--- scripts/firebase_deploy.sh
+++ scripts/firebase_deploy.sh
@@ -3,7 +3,7 @@
 # The following deploys the firebase function
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    npm run deploy
+    npm run --prefix services/${NAME} deploy
 else
     echo "Skipping deploy because not on master branch"
 fi
